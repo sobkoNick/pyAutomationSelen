7eb49e59719cc3e0093ee8b12c069bd71a3ef411
Mykola Sobko <sobko.nick@gmail.com>
2024-02-21 23:00:58 +0200

    added steps and gitignore file

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,4 @@
+from reportportal_client import step
 from selene import browser
 
 import utils.config_util
@@ -10,17 +11,21 @@ class LoginPage:
         self.password_input = by_css('#content-desktop #user_password')
         self.sing_in_btn = by_css('#content-desktop [type="submit"]')
 
+    @step
     def open_login_page(self):
         browser.open(utils.config_util.get_config('login_url'))
         return self
 
+    @step
     def fill_email(self, email):
         self.email_input.set(email)
         return self
 
+    @step
     def fill_password(self, password):
         self.password_input.set(password)
         return self
 
+    @step
     def press_sign_in(self):
         self.sing_in_btn.click()
