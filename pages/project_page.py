792365943435f571f38813b6f69da2fdef4520f1
Mykola Sobko <sobko.nick@gmail.com>
2024-03-21 22:45:27 +0200

    added locators and methods to create a test inside suite.

--- pages/project_page.py
+++ pages/project_page.py
@@ -10,6 +10,13 @@ class ProjectPage:
         self.add_suite_btn = s(by.xpath('//button[text()="Suite"]'))
         self.suite_item = '//p[@class="nestedItem-title"]//span[text()="{suite_name}"]'
         self.opened_suite_title = s(by.css('[class="detailed-view-suite-body"] h3'))
+        # Test fragment locators
+        # todo create a fragment for this?
+        self.add_new_test_btn = s(by.xpath('//a[contains(@href, "/new-test")]'))
+        self.test_name_input = s(by.css('[placeholder="Title"]'))
+        self.save_test_btn = s(by.xpath('//div[@class="detail-view-actions"]//button'))
+        self.requirements_input = s(by.xpath('//*[@class="view-line"][2]'))
+        self.steps_input = s(by.xpath('//*[@class="view-line"][4]'))
 
     @step
     def set_suite_name(self, name):
@@ -29,4 +36,31 @@ class ProjectPage:
     @step
     def verify_opened_suite_title(self, suite_title):
         self.opened_suite_title.should(have.exact_text(suite_title))
+        return self
+
+    @step
+    def click_to_add_new_test(self):
+        self.add_new_test_btn.click()
+        return self
+
+    @step
+    def set_test_name(self, name):
+        self.test_name_input.set(name)
+        return self
+
+    @step
+    def set_test_requirements(self, requirements_text):
+        # todo maybe it should set values from requirements_text like it's a list.
+        self.requirements_input.set(requirements_text)
+        return self
+
+    @step
+    def set_test_steps(self, steps_text):
+        # todo and here also. check while testing
+        self.steps_input.set(steps_text)
+        return self
+
+    @step
+    def save_test(self):
+        self.save_test_btn.click()
         return self
