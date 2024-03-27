f1b62eac9da22d8403d4866b11152da823ffd469
Mykola Sobko <sobko.nick@gmail.com>
2024-03-27 12:04:24 +0200

    automating test creation functionality.

--- pages/project_page.py
+++ pages/project_page.py
@@ -1,4 +1,5 @@
 from reportportal_client import step
+from selene import browser
 from selene.support import by
 from selene.support.conditions import have
 from selene.support.shared.jquery_style import s
@@ -12,6 +13,8 @@ class ProjectPage:
         self.opened_suite_title = s(by.css('[class="detailed-view-suite-body"] h3'))
         # Test fragment locators
         # todo create a fragment for this?
+        self.test_data_frame = s(by.css('[class="frame-container"] iframe'))
+        self.file_suite_type = s(by.css('li[role="radio"]'))
         self.add_new_test_btn = s(by.xpath('//a[contains(@href, "/new-test")]'))
         self.test_name_input = s(by.css('[placeholder="Title"]'))
         self.save_test_btn = s(by.xpath('//div[@class="detail-view-actions"]//button'))
@@ -39,6 +42,11 @@ class ProjectPage:
         return self
 
     @step
+    def choose_file_suite_type(self):
+        self.file_suite_type.click()
+        return self
+
+    @step
     def click_to_add_new_test(self):
         self.add_new_test_btn.click()
         return self
@@ -51,13 +59,30 @@ class ProjectPage:
     @step
     def set_test_requirements(self, requirements_text):
         # todo maybe it should set values from requirements_text like it's a list.
-        self.requirements_input.set(requirements_text)
+        # browser.execute_script("document.querySelector('[class=\"view-line\"] span span').setAttribute('value', 'your value here')")
+
+        # this works ->
+        # browser.execute_script("document.querySelector('[class=\"view-line\"]:nth-child(2) span span').textContent='newtext'")
+
+        # todo add check if frame is already selected
+        browser.switch_to.frame(1)
+        script = f"document.querySelector('[class=\"view-line\"]:nth-child(2) span span')" \
+                 f".textContent='{requirements_text}'"
+        browser.driver.execute_script(script)
+        # self.requirements_input.set(requirements_text)
         return self
 
     @step
     def set_test_steps(self, steps_text):
         # todo and here also. check while testing
-        self.steps_input.set(steps_text)
+
+        # todo add check if frame is already selected
+        script = f"document.querySelector('[class=\"view-line\"]:nth-child(4) span span')" \
+                 f".textContent='{steps_text}'"
+        browser.driver.execute_script(script)
+
+        # self.steps_input.set(steps_text)
+        # browser.switch_to.default_content()
         return self
 
     @step
---
+++ tests/test_test_functionality.py
@@ -0,0 +1,60 @@
+import json
+
+from _pytest.fixtures import fixture
+
+import utils.config_util
+from api_steps.api_client import ApiClient
+from constants.endpoint_names import SUITES_ENDPOINT
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+from pages.project_page import ProjectPage
+
+
+#   ---FIXTURES---
+
+@fixture(autouse=True)
+def perform_login(app):
+    """
+    Test precondition to perform login
+    """
+    LoginPage() \
+        .open_login_page() \
+        .fill_email(utils.config_util.get_config("username")) \
+        .fill_password(utils.config_util.get_config("password")) \
+        .press_sign_in()
+
+
+@fixture
+def existing_suite(app):
+    with open("tests/test_data/existing_suite.json") as json_data:
+        data = json.load(json_data)
+
+    # creates default suite for tests
+    suite = ApiClient(token=app.token, endpoint=SUITES_ENDPOINT, logger=app.logger) \
+        .post(url_params=[app.project_id], new_obj=data) \
+        .validate_that().status_code_is_ok().get_response_body()
+    # suite = Suite.build(json.dumps(suite['data']))
+
+    # passes suite name
+    yield suite['data']['attributes']['title']
+
+    app.logger.info("Deleting existing suite after test run")
+    ApiClient(token=app.token, endpoint=SUITES_ENDPOINT, logger=app.logger) \
+        .delete(url_params=[app.project_id, suite['data']['id']]).validate_that()
+
+
+#   ---TESTS---
+
+def test_adding_test_case_to_suite(app, existing_suite):
+    """
+    Test adds a new test to existing suite and verifies data was saved correctly
+    """
+    HomePage().open_project(app.project_name)
+    ProjectPage() \
+        .choose_suite(existing_suite) \
+        .choose_file_suite_type() \
+        .click_to_add_new_test()\
+        .set_test_name("created by autotest")\
+        .set_test_requirements("req1")\
+        .set_test_steps("step1")\
+        .save_test()
